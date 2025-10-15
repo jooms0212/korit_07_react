9f2a0b6abf1442ca09450ddfd712942f1af0402b
jooms0212 <jooms0212@naver.com>
2025-10-15 12:35:02 +0900

    feat:20251015

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import Counter from './Counter.jsx'
+import Counter3 from './Counter3.jsx'
 
 function App() {
 
   return (
     <div>
-      <Counter/>
+      <Counter3/>
     </div>
   )
 }
